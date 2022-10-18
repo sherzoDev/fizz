0035fd88e2519fa54f0069c52589acfc224047c8
sherzoDev <mustafoqulovsherzodbek@gmail.com>
2022-10-18 14:37:17 +0500

    Intial commit

--- main.js
+++ main.js
@@ -2,24 +2,18 @@ var elForm = document.querySelector(".form");
 var elInput = document.querySelector(".form-control");
 var elResult = document.querySelector(".form-result");
 var elBtn = document.querySelector(".btn");;
-
 var elFizz = document.querySelector(".fizz");
 var elBuzz = document.querySelector(".buzz");
 var elFizzBuzz = document.querySelector(".fizzbuzz");
-
 var fizzLength = document.querySelector(".fizzLength");
 var buzzLength = document.querySelector(".buzzLength");
 var fizzBuzzLength = document.querySelector(".fizzBuzzLength");
-
-
 var fizArr = [];
 var buzArr = [];
 var fizbuzArr = [];
-
 elForm.addEventListener("submit", function(evt){
     evt.preventDefault();
     var elInputValue = elInput.value;
-
     if(elInputValue % 5 == 0 && elInputValue % 3 == 0){
         fizbuzArr.push(elInputValue);
         elFizzBuzz.textContent = fizbuzArr;
@@ -32,7 +26,7 @@ elForm.addEventListener("submit", function(evt){
         buzArr.push(elInputValue);
         elBuzz.textContent = buzArr;
         buzzLength.textContent = buzArr.length;
-    }
-    else{
+    } else{
+        false;
     };
 });
